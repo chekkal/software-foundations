0aa531cf6a38e60c75f4cdf40097a77c84e87591
chekkal <chekkal@gmail.com>
2016-05-02 22:25:50 +0100

    Exercice * hd_opt_poly

---
+++ chapter6_Library_Poly/hd_opt_poly.v
@@ -0,0 +1,31 @@
+(* Software Foundations *)
+(* Exercice 1 star, hd_opt_poly *)
+
+Inductive list(X: Type): Type :=
+|nil: list X
+|cons: X -> list X -> list X.
+
+Arguments nil {X}.
+Arguments cons {X} _ _.
+
+Notation "[]" := nil.
+Notation "h :: t" := (cons h t)(at level 60, right associativity).
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..)(at level 60, right associativity).
+
+Inductive option(X: Type): Type:=
+|Some: X -> option X
+|None: option X.
+
+Arguments Some {X} _.
+Arguments None {X}.
+
+Definition hd_opt{X: Type}(l: list X): option X:=
+    match l with
+    |[]   => None
+    |h::_ => Some h
+    end.
+
+Check @hd_opt.
+
+Example test_hd_opt1: hd_opt ([1;2]) = Some 1. Proof. reflexivity. Qed.
+Example test_hd_opt2: hd_opt ([[1];[2]]) = Some ([1]). Proof. reflexivity. Qed.
