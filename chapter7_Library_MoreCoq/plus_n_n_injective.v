65fc4641946ce36bd1f1ed215b308d7d803013b7
chekkal <chekkal@gmail.com>
2016-05-07 13:26:17 +0100

    Exercice *** plus_n_n_injective

---
+++ chapter7_Library_MoreCoq/plus_n_n_injective.v
@@ -0,0 +1,18 @@
+(* Software Foundations *)
+(* Exercice 3 stars, plus_n_n_injective *)
+(* SearchAbout "S".
+
+Require Import Logic.
+
+Definition eq_S := f_equal S.*)
+
+Theorem plus_n_n_injective: forall n m, n+n=m+m -> n = m.
+Proof.
+intros n. induction n as [|n'].
+destruct m as [|m'].
+  reflexivity.
+  intros H. inversion H.
+destruct m as [|m'].
+  intros H. simpl in H. inversion H.
+  intros H. apply eq_S. apply IHn'. simpl in H. apply eq_add_S in H. rewrite <- plus_n_Sm in H. rewrite <- plus_n_Sm in H. apply eq_add_S in H. apply H.
+Qed.
