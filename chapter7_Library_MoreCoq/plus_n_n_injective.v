422c835ea06755dc61845d4c92495a43779afe8a
chekkal <chekkal@gmail.com>
2016-05-07 18:24:43 +0100

    Exercice *** plus_n_n_injective'

--- chapter7_Library_MoreCoq/plus_n_n_injective.v
+++ chapter7_Library_MoreCoq/plus_n_n_injective.v
@@ -16,3 +16,15 @@ destruct m as [|m'].
   intros H. simpl in H. inversion H.
   intros H. apply eq_S. apply IHn'. simpl in H. apply eq_add_S in H. rewrite <- plus_n_Sm in H. rewrite <- plus_n_Sm in H. apply eq_add_S in H. apply H.
 Qed.
+
+Theorem plus_n_n_injective': forall n m, n+n=m+m -> n = m.
+Proof.
+intros n. induction n as [|n'].
+destruct m as [|m'].
+  simpl. reflexivity.
+  simpl. intros contra. inversion contra.
+destruct m as [|m'].
+  simpl. intros contra. inversion contra.
+  intros H. apply eq_S. apply IHn'. rewrite plus_Sn_m in H. rewrite plus_Sn_m in H.
+  apply eq_add_S in H. rewrite <- plus_n_Sm in H. rewrite <- plus_n_Sm in H. apply eq_add_S in H. apply H.
+Qed.
